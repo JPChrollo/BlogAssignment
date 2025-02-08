c06122a743baa7ce6231790831e742fed0adc4c3
Pink <jpink@deloitte.co.uk>
2025-02-08 23:49:20 +0000

    add update user form

--- blogapp/users/forms.py
+++ blogapp/users/forms.py
@@ -1,6 +1,11 @@
+from flask_login import current_user
+from flask_migrate import current
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
 from wtforms import PasswordField, StringField, SubmitField, ValidationError, validators
 from wtforms.validators import DataRequired, Email, EqualTo
+
+from blogapp.models import User
 
 
 class LoginForm(FlaskForm):
@@ -29,3 +34,12 @@ class RegistrationForm(FlaskForm):
     def check_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError("Your username has been registered already!")
+
+class UpdateUserForm(FlaskForm):
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    username = StringField("UserName", validators=[DataRequired()])
+    picture = FileField(
+        "Update profile picture", validators=[FileAllowed(["jpg", "png"])]
+    )
+    submit = SubmitField("Update")
